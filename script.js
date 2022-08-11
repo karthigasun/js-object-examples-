4d4e32434980eb1173aefc546b66a7cab9fb9cee
karthigasun <prkarthik3@gmail.com>
2022-08-11 16:41:12 +0530

    js-object-examples

---
+++ script.js
@@ -0,0 +1,128 @@
+//Object Examples
+const a={Brand:"Maruthi",Name:"Swift",Color:"Red",FuelType:"Diesel"};
+console.log("The car is",a.Brand,a.Name,a.Color,a.FuelType);
+console.log(a);
+
+const b={Brand:"Hero",Name:"Splender",FuelType:"Petrol",Color:"Blue and black"};
+console.log("The bike is",b.Brand,b.Name,b.FuelType,b.Color);
+console.log(b);
+
+const c={Brand:"Teak",Name:"Wood",lockType:"Key",Color:"Maroon"};
+console.log("The Door is",c.Brand,c.Name,c.lockType,c.Color);
+console.log(c);
+
+const d={Brand:"Steel",Name:"Stainless Steel",lockType:"Push and Pull",Color:"Silver"};
+console.log("The Door is",d.Brand,d.Name,d.lockType,d.Color);
+console.log(d);
+
+const e={Brand:"Redmi",Name:"Note 8",Ram:"4GB",Color:"Black"};
+console.log("My Mobile is",e.Brand,e.Name,e.Ram,e.Color);
+console.log(e);
+
+const f={Brand:"Lenovo",Name:"Lenovo E-415-25",Ram:"4GB",Color:"Black"};
+console.log("My Laptop is",f.Brand,f.Name,f.Ram,f.Color);
+console.log(f);
+
+const g={Brand:"DELL",Name:"Inspiron 5500",Ram:"4GB",Color:"Gunmetal"};
+console.log("Harish Laptop is",g.Brand,g.Name,g.Ram,g.Color);
+console.log(g);
+
+const h={Brand:"Tata",Name:"Bus",FuelType:"Diesel",Color:"Red and Yellow"};
+console.log("Kerala have a",h.Brand,h.Name,h.FuelType,h.Color);
+console.log(h);
+
+const i={Brand:"Ashok Leyland",Name:"Bus",FuelType:"Diesel",Color:"Blue and Lightwhite"};
+console.log("TamilNadu have a",i.Brand,i.Name,i.FuelType,i.Color);
+console.log(i);
+
+const j={Brand:"Tesla",Name:"cybertuck",FuelType:"Electric",Color:"White"};
+console.log("The Car is",j.Brand,j.Name,j.FuelType,j.Color);
+console.log(i);
+
+const k={Brand:"Toyoto",Name:"Fortuner",FuelType:"Petrol",Color:"White"};
+console.log("The Car is",k.Brand,k.Name,k.FuelType,k.Color);
+console.log(k);
+
+const l={Brand:"Toyoto",Name:"Innovo",FuelType:"Petrol",Color:"White"};
+console.log("The Car is",l.Brand,k.Name,k.FuelType,k.Color);
+console.log(l);
+
+const m={Brand:"Mahindra",Name:"Scorpio",FuelType:"Petrol",Color:"Black"};
+console.log("The Car is",m.Brand,m.Name,m.FuelType,m.Color);
+console.log(m);
+
+const n={Brand:"Mahindra",Name:"Bolero",FuelType:"Petrol",Color:"Metal"};
+console.log("The Car is",n.Brand,n.Name,n.FuelType,n.Color);
+console.log(n);
+
+const o={Name:"Dhoni",best:"Wicket keeper",Type:"Cricket"};
+console.log(" The Role of",o.Name,o.best,o.Type);
+console.log(o);
+
+const p={Name:"Mr",Role:"IPL",Type:"Cricket"};
+console.log(" Raina is",p.Name,p.Role,p.Type);
+console.log(p);
+
+const q={Name:"Boult",Role:"Headset",Type:"wireless"};
+console.log(" My ",q.Name,q.Role,q.Type);
+console.log(q);
+
+const r={Name:"Oppo",Role:"Multiple",Type:"Android"};
+console.log(" My Old Mobile ",r.Name,r.Type);
+console.log(r);
+
+const s={Name:"Honor 6s",Role:"Multiple",Type:"Android"};
+console.log(" Harish Old Mobile ",s.Name,s.Type);
+console.log(s);
+
+const t={Name:"Cricket",Role:"Multiple",Type:"All Formats"};
+console.log(" My favourite game is ",t.Name,t.Type);
+console.log(t);
+
+const u={Name:"Reliance",Role:"Give the air",Type:"1200RPM"};
+console.log(" The Ceilng Fan ",u.Name,u.Type);
+console.log(u);
+
+const v={Name:"Crompton",Role:"Give the brightness",Type:"LED"};
+console.log(" The Tubelight ",v.Name,v.Type);
+console.log(v);
+
+const w={Name:"Winner",Role:"Wifi ",Type:"Wireless"};
+console.log(" The Bradband is ",w.Name,w.Type);
+console.log(w);
+
+const x={Name:"Redmi",Role:"Android",Type:"7A"};
+console.log(" Selva Sir have ",x.Name,x.Type);
+console.log(x);
+
+const y={Name:"Boat",Role:"Headset",Type:"wireless"};
+console.log(" ManiRaj have ",y.Name,y.Role);
+console.log(y);
+
+const z={Name:"Apsara",Role:"Writing and Drawing",Type:"Corbon"};
+console.log("The Pencil ",z.Name,z.Role);
+console.log(z);
+
+const aa={Name:"Prolite",Role:"Typing",Type:"Wired"};
+console.log("The Keyboard ",aa.Name,aa.Role);
+console.log(aa);
+
+const ba={Name:"POCO",Role:"Multiple",Type:"F3"};
+console.log("The Keyboard ",ba.Name,ba.Type);
+console.log(ba);
+
+const ca={Name:"Realme",Role:"Multiple",Type:"X2"};
+console.log("Maniraj have ",ca.Name,ca.Type);
+console.log(ca);
+
+const da={Name:"Vivo",Role:"Multiple",Type:"V20"};
+console.log("Vairadurai have ",da.Name,da.Type);
+console.log(da);
+
+
+
+
+
+
+
+
